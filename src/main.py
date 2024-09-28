1b13e1013c9845a38c3ff2f4aedec874d9e7f4fb
gbaliarda <gbaliarda@itba.edu.ar>
2024-09-28 14:45:40 -0300

    chore: add jupyter notebook dependency

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 from langchain_ollama import ChatOllama
 
 model = ChatOllama(
-    model="codellama",
+    model="llama3.1:8b",
 )
 
 response_message = model.invoke(
-    "Do you know the Solidity language?"
+    "Can you create a basic ERC20 contract with all the methods implemented?"
 )
 
 print(response_message.content)
